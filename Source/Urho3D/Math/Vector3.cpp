fb4586ed88baad53ed4070dac2e05027d83a28cd
Casey McCandless <caseymcc@krazer.net>
2017-07-01 09:51:08 -0500

    alter coordinate system

--- Source/Urho3D/Math/Vector3.cpp
+++ Source/Urho3D/Math/Vector3.cpp
@@ -32,22 +32,39 @@ namespace Urho3D
 {
 
 const Vector3 Vector3::ZERO;
+const Vector3 Vector3::ONE(1.0f, 1.0f, 1.0f);
+const IntVector3 IntVector3::ZERO;
+const IntVector3 IntVector3::ONE(1, 1, 1);
+#elif defined URHO3D_RH_YXZ //Right Hand Y-forward X-right Z-up
+const Vector3 Vector3::LEFT(-1.0f, 0.0f, 0.0f);
+const Vector3 Vector3::RIGHT(1.0f, 0.0f, 0.0f);
+const Vector3 Vector3::UP(0.0f, 0.0f, 1.0f);
+const Vector3 Vector3::DOWN(0.0f, 0.0f, -1.0f);
+const Vector3 Vector3::FORWARD(0.0f, 1.0f, 0.0f);
+const Vector3 Vector3::BACK(0.0f, -1.0f, -0.0f);
+
+const IntVector3 IntVector3::LEFT(-1, 0, 0);
+const IntVector3 IntVector3::RIGHT(1, 0, 0);
+const IntVector3 IntVector3::UP(0, 0, 1);
+const IntVector3 IntVector3::DOWN(0, 0, -1);
+const IntVector3 IntVector3::FORWARD(0, 1, 0);
+const IntVector3 IntVector3::BACK(0, -1, 0);
+#else //Default URHO3D_LH_ZXY Left Hand Z-forward X-right Y-up
 const Vector3 Vector3::LEFT(-1.0f, 0.0f, 0.0f);
 const Vector3 Vector3::RIGHT(1.0f, 0.0f, 0.0f);
 const Vector3 Vector3::UP(0.0f, 1.0f, 0.0f);
 const Vector3 Vector3::DOWN(0.0f, -1.0f, 0.0f);
 const Vector3 Vector3::FORWARD(0.0f, 0.0f, 1.0f);
 const Vector3 Vector3::BACK(0.0f, 0.0f, -1.0f);
-const Vector3 Vector3::ONE(1.0f, 1.0f, 1.0f);
 
-const IntVector3 IntVector3::ZERO;
 const IntVector3 IntVector3::LEFT(-1, 0, 0);
 const IntVector3 IntVector3::RIGHT(1, 0, 0);
 const IntVector3 IntVector3::UP(0, 1, 0);
 const IntVector3 IntVector3::DOWN(0, -1, 0);
 const IntVector3 IntVector3::FORWARD(0, 0, 1);
 const IntVector3 IntVector3::BACK(0, 0, -1);
-const IntVector3 IntVector3::ONE(1, 1, 1);
+#endif
+
 
 String Vector3::ToString() const
 {
